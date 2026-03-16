eb551c4d2a5ce52f683e0d5bda85e9806ac33736
ozziekn <ozzieknowles2@gmail.com>
2026-03-16 05:46:51 -0500

    Update spike_analysis.py
    Add leaky integrate-and-fire neuron simulation

--- spike_analysis.py
+++ spike_analysis.py
@@ -1,1 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+dt = 0.1
+T = 100
+time = np.arange(0, T, dt)
+
+tau = 20
+V_rest = -65
+V_threshold = -50
+V_reset = -65
+
+V = np.zeros(len(time))
+V[0] = V_rest
+
+input_current = 1.5
+
+for i in range(1, len(time)):
+    dV = (-(V[i-1] - V_rest) + input_current) / tau
+    V[i] = V[i-1] + dt * dV
+
+    if V[i] >= V_threshold:
+        V[i] = V_reset
+
+plt.plot(time, V)
+plt.title("Leaky Integrate-and-Fire Neuron")
+plt.xlabel("Time")
+plt.ylabel("Membrane Voltage (mV)")
+plt.show()
